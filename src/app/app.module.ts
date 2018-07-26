3a888153666e1566a52f51419dd1b5345807351c
JS Galvis <jsgalvish@unal.edu.co>
2018-07-25 23:04:50 -0500

    ng generate component sidebar / post / users / details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PostComponent } from './post/post.component';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PostComponent,
+    UsersComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
