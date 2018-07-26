6ad5f487af6ae92eb49e40bffdb05de5009dfaec
JS Galvis <jsgalvish@unal.edu.co>
2018-07-25 23:58:37 -0500

    Details id method

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,8 +8,15 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers() {
-    return this.http.get('https://jsonplaceholder.typicode.com/users')
-  }
+	getUsers() {
+		return this.http.get('https://jsonplaceholder.typicode.com/users')
+	}
 
+	getUser(userId) {
+		return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
+	}
+
+	getPosts() {
+		return this.http.get('https://jsonplaceholder.typicode.com/posts')
+	}
 }
